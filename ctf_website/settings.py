fea7c5e58733de6e4ccee22a30d9e1c14aef1b2d
sreekumar <sreekumar.c@qburst.com>
2023-06-30 14:49:10 +0530

    Second commit

--- ctf_website/settings.py
+++ ctf_website/settings.py
@@ -125,3 +125,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks kike no local file")
